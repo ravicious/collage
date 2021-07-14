30efb0ad30272239ae7c179d9fb1daea3915206a
Rafał Cieślak <ravicious@gmail.com>
2021-07-14 13:20:33 +0200

    Fix image orientation

--- image-processor/src/orientation.rs
+++ image-processor/src/orientation.rs
@@ -1,66 +1,79 @@
 use exif::{In, Tag};
-use image::RgbImage;
+use image::{imageops, RgbImage};
 use std::io::Cursor;
 use web_sys::console;
 
 pub fn fix_if_needed(raw_image: Vec<u8>) -> RgbImage {
     let orientation = get_orientation(&raw_image);
-    let needs_to_be_fixed = needs_fixing(orientation);
-    console::log_1(&format!("needs to be fixed? {:?}", needs_to_be_fixed).into());
-    // TODO: Actually fix the image before returning it.
-    image::load_from_memory(&raw_image).unwrap().into_rgb8()
+    let image = image::load_from_memory(&raw_image).unwrap().into_rgb8();
+
+    match orientation {
+        Ok(orientation_tag) => fix_orientation(image, orientation_tag),
+        Err(reason) => {
+            log_reason_for_no_orientation_fix(reason);
+            image
+        }
+    }
 }
 
 #[derive(Debug)]
-enum OrientationError {
+enum NoFixNeededReason {
+    AleadyCorrect,
     ParsingError(String),
     NoExif,
     NoOrientationTag,
+    InvalidOrientationTagValue(Option<u32>),
 }
 
-fn get_orientation(raw_image: &[u8]) -> Result<exif::Field, OrientationError> {
+fn get_orientation(raw_image: &[u8]) -> Result<u32, NoFixNeededReason> {
     let reader = exif::Reader::new();
     let mut cursor = Cursor::new(&raw_image);
     let exif_data = reader
         .read_from_container(&mut cursor)
         .map_err(|err| match err {
-            exif::Error::NotFound(_) => OrientationError::NoExif,
-            _ => OrientationError::ParsingError(format!("{:?}", err)),
+            exif::Error::NotFound(_) => NoFixNeededReason::NoExif,
+            _ => NoFixNeededReason::ParsingError(format!("{:?}", err)),
         })?;
-
-    exif_data
+    let exif_field = exif_data
         .get_field(Tag::Orientation, In::PRIMARY)
-        .cloned()
-        .ok_or(OrientationError::NoOrientationTag)
+        .ok_or(NoFixNeededReason::NoOrientationTag)?;
+
+    match exif_field.value.get_uint(0) {
+        Some(1) => Err(NoFixNeededReason::AleadyCorrect),
+        Some(value @ 2..=8) => Ok(value),
+        other => Err(NoFixNeededReason::InvalidOrientationTagValue(other)),
+    }
 }
 
-fn needs_fixing(field_result: Result<exif::Field, OrientationError>) -> bool {
-    match field_result {
-        Ok(orientation) => match orientation.value.get_uint(0) {
-            Some(1) => false,
-            Some(2..=8) => true,
-            value => {
-                console::error_1(
-                    &format!("Value of orientation field is invalid: {:?}", value).into(),
-                );
-                false
-            }
-        },
-        Err(error) => {
-            match error {
-                OrientationError::NoExif => {
-                    console::log_1(&"No Exif data found".into());
-                }
-                OrientationError::NoOrientationTag => {
-                    console::log_1(&"No orientation tag found in Exif attributes".into());
-                }
-                _ => {
-                    console::error_1(
-                        &format!("Error while getting orientation from Exif: {:?}", error).into(),
-                    );
-                }
-            }
-            false
-        }
+fn log_reason_for_no_orientation_fix(reason: NoFixNeededReason) {
+    use NoFixNeededReason::*;
+
+    let log_function = match reason {
+        AleadyCorrect | NoExif | NoOrientationTag => console::log_1,
+        ParsingError(_) | InvalidOrientationTagValue(_) => console::error_1,
+    };
+
+    log_function(&format!("{:?}", reason).into());
+}
+
+// Naive implementation until I figure out how to use transformation matrices with the image crate.
+fn fix_orientation(mut image: RgbImage, orientation: u32) -> RgbImage {
+    if orientation > 8 {
+        return image;
     }
+
+    if orientation >= 5 {
+        image = imageops::rotate90(&image);
+        imageops::flip_horizontal_in_place(&mut image);
+    }
+
+    if orientation == 3 || orientation == 4 || orientation == 7 || orientation == 8 {
+        imageops::rotate180_in_place(&mut image);
+    }
+
+    if orientation % 2 == 0 {
+        imageops::flip_horizontal_in_place(&mut image);
+    }
+
+    image
 }
--- init.js
+++ init.js
@@ -16,6 +16,8 @@ app.ports.sendImagesToJs.subscribe((files) => {
 })
 
 const generateCollage = async (files) => {
+  document.getElementById('result').src = "";
+
   const imageArray1 = new Uint8Array(await files[0].arrayBuffer());
   const imageArray2 = new Uint8Array(await files[1].arrayBuffer());
 
@@ -27,3 +29,22 @@ const generateCollage = async (files) => {
 
   app.ports.imageProcessorStatus.send("done");
 }
+
+const orientationTest = async () => {
+  const loadFile = (n) => fetch(`vendor/orientation-test/f${n}t.jpg`)
+    .then((response) => response.blob())
+    .then((blob) => blob.arrayBuffer())
+    .then((buffer) => new Uint8Array(buffer));
+  const pairs = [[1, 2], [3, 4], [5, 6], [7, 8]];
+
+  for (const [n1, n2] of pairs) {
+    const result = process_images(await loadFile(n1), await loadFile(n2));
+    const img = document.createElement('img');
+    img.src = URL.createObjectURL(
+      new Blob([result.buffer], {type: 'image/jpg'})
+    );
+
+    document.body.appendChild(img);
+  }
+}
+window.orientatationTest = orientationTest;
