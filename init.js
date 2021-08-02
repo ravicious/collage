ee133f22bc324fb407e74d22742708e27c5229d8
Rafał Cieślak <ravicious@gmail.com>
2021-08-02 12:23:03 +0200

    Move Wasm processing to a web worker
    This should avoid making browsers think that the web page is not responding.

--- init.js
+++ init.js
@@ -1,11 +1,7 @@
 'use strict';
 
-import {default as initWasm, setup, process_images} from './image-processor/pkg/image_processor.js';
-
-(async () => {
-  await initWasm();
-  setup();
-})();
+const worker = new Worker('worker.js');
+const resultImg = document.getElementById('result')
 
 const app = Elm.Main.init({
   node: document.getElementById('app-container')
@@ -15,7 +11,6 @@ app.ports.sendImagesToJs.subscribe((files) => {
   generateCollage(files).catch(console.error)
 })
 
-const resultImg = document.getElementById('result')
 resultImg.onload = function() {
   console.log('revoking Object URL')
   URL.revokeObjectURL(this.src);
@@ -28,7 +23,7 @@ const generateCollage = async (files) => {
   const imageArray2 = new Uint8Array(await files[1].arrayBuffer());
 
   console.time('process_images');
-  const resultArray = process_images(imageArray1, imageArray2);
+  const resultArray = await process_images(imageArray1, imageArray2);
   console.timeEnd('process_images');
 
   resultImg.src = URL.createObjectURL(
@@ -38,7 +33,18 @@ const generateCollage = async (files) => {
   app.ports.imageProcessorStatus.send("done");
 }
 
+// Wrapping the message passing in a promise.
+// The worker code is simple enough that we can let ourselves do that.
+const process_images = (imageArray1, imageArray2) => new Promise((resolve, reject) => {
+  worker.onmessage = (event) => {
+    resolve(event.data)
+  }
+  worker.postMessage([imageArray1, imageArray2], [imageArray1.buffer, imageArray2.buffer])
+})
+
 const orientationTest = async () => {
+  console.time('orientation test');
+
   const loadFile = (n) => fetch(`vendor/orientation-test/f${n}t.jpg`)
     .then((response) => response.blob())
     .then((blob) => blob.arrayBuffer())
@@ -46,13 +52,17 @@ const orientationTest = async () => {
   const pairs = [[1, 2], [3, 4], [5, 6], [7, 8]];
 
   for (const [n1, n2] of pairs) {
-    const result = process_images(await loadFile(n1), await loadFile(n2));
+    const result = await process_images(await loadFile(n1), await loadFile(n2));
     const img = document.createElement('img');
+    img.onload = function() {
+      URL.revokeObjectURL(this.src);
+    }
     img.src = URL.createObjectURL(
       new Blob([result.buffer], {type: 'image/jpg'})
     );
 
     document.body.appendChild(img);
   }
+
+  console.timeEnd('orientation test');
 }
-window.orientatationTest = orientationTest;
