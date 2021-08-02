c99db0acabd1c9375eda331ec8bcaa8de9165f85
Rafał Cieślak <ravicious@gmail.com>
2021-08-02 17:22:41 +0200

    Publish updated version

--- image-processor/pkg/image_processor.js
+++ image-processor/pkg/image_processor.js
@@ -1,1 +1,252 @@
-let wasm_bindgen;!function(){const e={};let l,t=new TextDecoder("utf-8",{ignoreBOM:!0,fatal:!0});t.decode();let n=null;function b(){return null!==n&&n.buffer===l.memory.buffer||(n=new Uint8Array(l.memory.buffer)),n}function r(e,n){return t.decode(b().subarray(e,e+n))}const o=new Array(32).fill(void 0);o.push(void 0,null,!0,!1);let i=o.length;function a(e){i===o.length&&o.push(o.length+1);var n=i;return i=o[n],o[n]=e,n}function c(e){return o[e]}function s(e){var n=c(e);return(e=e)<36||(o[e]=i,i=e),n}e.setup=function(){l.setup()};let g=0;function w(e,n){n=n(+e.length);return b().set(e,+n),g=e.length,n}let u=null;function d(){return null!==u&&u.buffer===l.memory.buffer||(u=new Int32Array(l.memory.buffer)),u}e.process_images=function(e,n){try{var t=l.__wbindgen_add_to_stack_pointer(-16),r=w(e,l.__wbindgen_malloc),o=g,i=w(n,l.__wbindgen_malloc),a=g;l.process_images(t,r,o,i,a);var c=d()[t/4+0],s=d()[t/4+1],u=(f=c,_=s,b().subarray(+f,+f+_).slice());return l.__wbindgen_free(c,+s),u}finally{l.__wbindgen_add_to_stack_pointer(16)}var f,_};let f=new TextEncoder("utf-8");const _="function"==typeof f.encodeInto?function(e,n){return f.encodeInto(e,n)}:function(e,n){var t=f.encode(e);return n.set(t),{read:e.length,written:t.length}};async function m(n){if(void 0===n){let e;e="undefined"==typeof document?location.href:document.currentScript.src,n=e.replace(/\.js$/,"_bg.wasm")}const e={wbg:{}};e.wbg.__wbindgen_string_new=function(e,n){return a(r(e,n))},e.wbg.__wbindgen_object_drop_ref=function(e){s(e)},e.wbg.__wbg_new_59cb74e423758ede=function(){return a(new Error)},e.wbg.__wbg_stack_558ba5917b466edd=function(e,n){var t=function(e,n,t){if(void 0===t){var r=f.encode(e);const i=n(r.length);return b().subarray(i,i+r.length).set(r),g=r.length,i}let o=e.length,i=n(o);const a=b();let c=0;for(;c<o;c++){var s=e.charCodeAt(c);if(127<s)break;a[i+c]=s}return c!==o&&(0!==c&&(e=e.slice(c)),i=t(i,o,o=c+3*e.length),t=b().subarray(i+c,i+o),t=_(e,t),c+=t.written),g=c,i}(c(n).stack,l.__wbindgen_malloc,l.__wbindgen_realloc),n=g;d()[e/4+1]=n,d()[e/4+0]=t},e.wbg.__wbg_error_4bb6c2a97407129a=function(e,n){try{console.error(r(e,n))}finally{l.__wbindgen_free(e,n)}},e.wbg.__wbg_error_d95afd6217cfd219=function(e){console.error(c(e))},e.wbg.__wbg_log_9a99fb1af846153b=function(e){console.log(c(e))},e.wbg.__wbg_time_8897e55ebee4075f=function(e,n){console.time(r(e,n))},e.wbg.__wbg_timeEnd_6186a583833a7163=function(e,n){console.timeEnd(r(e,n))},e.wbg.__wbindgen_throw=function(e,n){throw new Error(r(e,n))};var{instance:t,module:n}=await async function(n,e){if("function"==typeof Response&&n instanceof Response){if("function"==typeof WebAssembly.instantiateStreaming)try{return WebAssembly.instantiateStreaming(n,e)}catch(e){if("application/wasm"==n.headers.get("Content-Type"))throw e;console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n",e)}var t=await n.arrayBuffer();return WebAssembly.instantiate(t,e)}return(e=await WebAssembly.instantiate(n,e))instanceof WebAssembly.Instance?{instance:e,module:n}:e}(await(n="string"==typeof n||"function"==typeof Request&&n instanceof Request||"function"==typeof URL&&n instanceof URL?fetch(n):n),e);return l=t.exports,m.__wbindgen_wasm_module=n,l}wasm_bindgen=Object.assign(m,e)}();
+let wasm_bindgen;
+(function() {
+    const __exports = {};
+    let wasm;
+
+    let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+    cachedTextDecoder.decode();
+
+    let cachegetUint8Memory0 = null;
+    function getUint8Memory0() {
+        if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
+            cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+        }
+        return cachegetUint8Memory0;
+    }
+
+    function getStringFromWasm0(ptr, len) {
+        return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+    }
+
+    const heap = new Array(32).fill(undefined);
+
+    heap.push(undefined, null, true, false);
+
+    let heap_next = heap.length;
+
+    function addHeapObject(obj) {
+        if (heap_next === heap.length) heap.push(heap.length + 1);
+        const idx = heap_next;
+        heap_next = heap[idx];
+
+        heap[idx] = obj;
+        return idx;
+    }
+
+function getObject(idx) { return heap[idx]; }
+
+function dropObject(idx) {
+    if (idx < 36) return;
+    heap[idx] = heap_next;
+    heap_next = idx;
+}
+
+function takeObject(idx) {
+    const ret = getObject(idx);
+    dropObject(idx);
+    return ret;
+}
+/**
+*/
+__exports.setup = function() {
+    wasm.setup();
+};
+
+let WASM_VECTOR_LEN = 0;
+
+function passArray8ToWasm0(arg, malloc) {
+    const ptr = malloc(arg.length * 1);
+    getUint8Memory0().set(arg, ptr / 1);
+    WASM_VECTOR_LEN = arg.length;
+    return ptr;
+}
+
+let cachegetInt32Memory0 = null;
+function getInt32Memory0() {
+    if (cachegetInt32Memory0 === null || cachegetInt32Memory0.buffer !== wasm.memory.buffer) {
+        cachegetInt32Memory0 = new Int32Array(wasm.memory.buffer);
+    }
+    return cachegetInt32Memory0;
+}
+
+function getArrayU8FromWasm0(ptr, len) {
+    return getUint8Memory0().subarray(ptr / 1, ptr / 1 + len);
+}
+/**
+* @param {Uint8Array} image_array1
+* @param {Uint8Array} image_array2
+* @returns {Uint8Array}
+*/
+__exports.process_images = function(image_array1, image_array2) {
+    try {
+        const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
+        var ptr0 = passArray8ToWasm0(image_array1, wasm.__wbindgen_malloc);
+        var len0 = WASM_VECTOR_LEN;
+        var ptr1 = passArray8ToWasm0(image_array2, wasm.__wbindgen_malloc);
+        var len1 = WASM_VECTOR_LEN;
+        wasm.process_images(retptr, ptr0, len0, ptr1, len1);
+        var r0 = getInt32Memory0()[retptr / 4 + 0];
+        var r1 = getInt32Memory0()[retptr / 4 + 1];
+        var v2 = getArrayU8FromWasm0(r0, r1).slice();
+        wasm.__wbindgen_free(r0, r1 * 1);
+        return v2;
+    } finally {
+        wasm.__wbindgen_add_to_stack_pointer(16);
+    }
+};
+
+let cachedTextEncoder = new TextEncoder('utf-8');
+
+const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
+    ? function (arg, view) {
+    return cachedTextEncoder.encodeInto(arg, view);
+}
+    : function (arg, view) {
+    const buf = cachedTextEncoder.encode(arg);
+    view.set(buf);
+    return {
+        read: arg.length,
+        written: buf.length
+    };
+});
+
+function passStringToWasm0(arg, malloc, realloc) {
+
+    if (realloc === undefined) {
+        const buf = cachedTextEncoder.encode(arg);
+        const ptr = malloc(buf.length);
+        getUint8Memory0().subarray(ptr, ptr + buf.length).set(buf);
+        WASM_VECTOR_LEN = buf.length;
+        return ptr;
+    }
+
+    let len = arg.length;
+    let ptr = malloc(len);
+
+    const mem = getUint8Memory0();
+
+    let offset = 0;
+
+    for (; offset < len; offset++) {
+        const code = arg.charCodeAt(offset);
+        if (code > 0x7F) break;
+        mem[ptr + offset] = code;
+    }
+
+    if (offset !== len) {
+        if (offset !== 0) {
+            arg = arg.slice(offset);
+        }
+        ptr = realloc(ptr, len, len = offset + arg.length * 3);
+        const view = getUint8Memory0().subarray(ptr + offset, ptr + len);
+        const ret = encodeString(arg, view);
+
+        offset += ret.written;
+    }
+
+    WASM_VECTOR_LEN = offset;
+    return ptr;
+}
+
+async function load(module, imports) {
+    if (typeof Response === 'function' && module instanceof Response) {
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            try {
+                return await WebAssembly.instantiateStreaming(module, imports);
+
+            } catch (e) {
+                if (module.headers.get('Content-Type') != 'application/wasm') {
+                    console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        const bytes = await module.arrayBuffer();
+        return await WebAssembly.instantiate(bytes, imports);
+
+    } else {
+        const instance = await WebAssembly.instantiate(module, imports);
+
+        if (instance instanceof WebAssembly.Instance) {
+            return { instance, module };
+
+        } else {
+            return instance;
+        }
+    }
+}
+
+async function init(input) {
+    if (typeof input === 'undefined') {
+        let src;
+        if (typeof document === 'undefined') {
+            src = location.href;
+        } else {
+            src = document.currentScript.src;
+        }
+        input = src.replace(/\.js$/, '_bg.wasm');
+    }
+    const imports = {};
+    imports.wbg = {};
+    imports.wbg.__wbindgen_string_new = function(arg0, arg1) {
+        var ret = getStringFromWasm0(arg0, arg1);
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbindgen_object_drop_ref = function(arg0) {
+        takeObject(arg0);
+    };
+    imports.wbg.__wbg_new_59cb74e423758ede = function() {
+        var ret = new Error();
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_stack_558ba5917b466edd = function(arg0, arg1) {
+        var ret = getObject(arg1).stack;
+        var ptr0 = passStringToWasm0(ret, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        var len0 = WASM_VECTOR_LEN;
+        getInt32Memory0()[arg0 / 4 + 1] = len0;
+        getInt32Memory0()[arg0 / 4 + 0] = ptr0;
+    };
+    imports.wbg.__wbg_error_4bb6c2a97407129a = function(arg0, arg1) {
+        try {
+            console.error(getStringFromWasm0(arg0, arg1));
+        } finally {
+            wasm.__wbindgen_free(arg0, arg1);
+        }
+    };
+    imports.wbg.__wbg_error_d95afd6217cfd219 = function(arg0) {
+        console.error(getObject(arg0));
+    };
+    imports.wbg.__wbg_log_9a99fb1af846153b = function(arg0) {
+        console.log(getObject(arg0));
+    };
+    imports.wbg.__wbg_time_8897e55ebee4075f = function(arg0, arg1) {
+        console.time(getStringFromWasm0(arg0, arg1));
+    };
+    imports.wbg.__wbg_timeEnd_6186a583833a7163 = function(arg0, arg1) {
+        console.timeEnd(getStringFromWasm0(arg0, arg1));
+    };
+    imports.wbg.__wbindgen_throw = function(arg0, arg1) {
+        throw new Error(getStringFromWasm0(arg0, arg1));
+    };
+
+    if (typeof input === 'string' || (typeof Request === 'function' && input instanceof Request) || (typeof URL === 'function' && input instanceof URL)) {
+        input = fetch(input);
+    }
+
+
+
+    const { instance, module } = await load(await input, imports);
+
+    wasm = instance.exports;
+    init.__wbindgen_wasm_module = module;
+
+    return wasm;
+}
+
+wasm_bindgen = Object.assign(init, __exports);
+
+})();
