85a4a040a020605c7fe4d4250cb0602bc93be4d4
Rafał Cieślak <ravicious@gmail.com>
2021-10-31 18:43:32 +0100

    Tweak algorithm params

--- image-processor/src/algorithm.rs
+++ image-processor/src/algorithm.rs
@@ -148,8 +148,10 @@ impl<'a> MutationOp<Layout<'a>> for LayoutMutation {
 }
 
 pub fn generate_layout(images: &[RgbImage]) -> Result<Layout, String> {
-    let population_size = 40;
-    let generation_limit = 2000;
+    // 49 is the max population size. For values bigger than that, genevo starts parallelizing the
+    // work by using rayon, which doesn't work OOTB for the Wasm target.
+    let population_size = 49;
+    let generation_limit = 4_000;
     let selection_ratio = 0.7;
     let num_individuals_per_parents = 2;
     let reinsertion_ratio = 0.7;
